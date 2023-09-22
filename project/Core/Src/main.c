efd5dea15bd5cbed50a673ab4ef0635963c2e31f
Kido <hoangphongvip2003@gmail.com>
2023-09-22 14:11:43 +0700

    fix lib

--- project/Core/Src/main.c
+++ project/Core/Src/main.c
@@ -17,11 +17,11 @@
  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "../MyLib/software_timer.h"
+
 #include "main.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "software_timer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
